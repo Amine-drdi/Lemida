cb04649a86bfbd6ac42cb7d2752d1df4bf98d7dc
amine <dridiimedamine@gmail.com>
2025-02-28 09:16:24 +0100

    modification footer

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
-/*import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-});*/
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true, // Ouvre automatiquement le navigateur à l'adresse du serveur
-    historyApiFallback: true, // Permet la gestion des routes SPA
-
+  base: "/", // S'assurer que les assets sont bien chargés
+  resolve: {
+    alias: {
+      "@": "/src", // Optionnel, mais utile pour éviter les imports relatifs longs
+    }
   },
+  build: {
+    outDir: "dist",
+  }
 });
